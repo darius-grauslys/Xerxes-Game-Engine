d48fce761bf6cbe06e70be7d0cebec8a05612569
WearsomeKarma <pahprios@gmail.com>
2022-02-28 13:06:48 -0500

    Branch Maintenance

--- XerxesUI/Xerxes_UI/UI_Horizontal_Anchor_Type.cs
+++
@@ -1,9 +0,0 @@
-namespace Xerxes_UI
-{
-    public enum UI_Horizontal_Anchor_Type
-    {
-        Top = 7,
-        Middle = 4,
-        Bottom = 1
-    }
-}
--- XerxesUI/Xerxes_UI/UI_Vertical_Anchor_Type.cs
+++
@@ -1,9 +0,0 @@
-namespace Xerxes_UI
-{
-    public enum UI_Vertical_Anchor_Type
-    {
-        Left = -1,
-        Middle = 0,
-        Right = 1
-    }
-}
